83142126515e1f6b49c15d70baa34721db1588f3
Richard Hallett <whitefire0@Richards-Air.default>
2018-01-01 09:30:13 +0000

    initial isMyUnique commit to create remote branch

---
+++ jasmine/src/01_arrays_and_strings/1.1 - Is Unique/myIsUnique.js
@@ -0,0 +1,33 @@
+/*
+ * "Cracking the Coding Interview  - Gayle Laakman McDowell"
+ * <name of challenge> <version number>
+ * Copyright 2017 whitefires0 / Rick Hallett
+ *
+ * Freely distributable under the creative commons license.
+ *
+ * Full details and documentation:
+ * http://www.crackingthecodinginterview.com/
+ */
+//====================================================================================================
+	/* --- Project Specification --- */
+//====================================================================================================
+
+/* Implement an algorithm to see if a string has all unique characters. 
+ * What if you cannot use additional data structures?*/
+    
+    
+//====================================================================================================
+    /* --- Project Implementation --- */
+//====================================================================================================
+
+/**
+ * Try drawing an example
+ * State/code the brute force option, type out potential stages
+ * Optimise if possible (sorting, data types)
+ * Stress testing
+ * Remove bottlenecks, unneccessary work, duplicate work
+ * If struggling, think of common sense, real world examples and how you might work them out as a human not a computer
+ * Simplify and generalise, start out with the most simple base cases and work upwards incrementallly
+ * 
+*/
+
--- jasmine/src/templateSrc.js
+++ jasmine/src/templateSrc.js
@@ -19,7 +19,14 @@
     /* --- Project Implementation --- */
 //====================================================================================================
 
-/*
-
+/**
+ * Try drawing an example
+ * State/code the brute force option, type out potential stages
+ * Optimise if possible (sorting, data types)
+ * Stress testing
+ * Remove bottlenecks, unneccessary work, duplicate work
+ * If struggling, think of common sense, real world examples and how you might work them out as a human not a computer
+ * Simplify and generalise, start out with the most simple base cases and work upwards incrementallly
+ * 
 */
 
